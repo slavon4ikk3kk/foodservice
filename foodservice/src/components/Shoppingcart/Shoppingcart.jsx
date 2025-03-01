6aa0a5997180605918a21edcf9a57a2e61727c27
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-03-01 12:26:44 +0200

    fix scroll shoppingcart

--- foodservice/src/components/Shoppingcart/Shoppingcart.jsx
+++ foodservice/src/components/Shoppingcart/Shoppingcart.jsx
@@ -5,19 +5,20 @@ import dishes from "../../Data/dishes.jsx";
 import ShoppingcartItem from "./ShoppingcartItem";
 
 const ShoppingCart = ({ setProductsList, productsList, setisOpen }) => {
-
   const totalCost = productsList.reduce((sum, product) => {
     const currentPrice = dishes.find((dish) => {
       return product.id === dish.id;
-    })
+    });
     return sum + product.amount * currentPrice.price;
-  }, 0)
-
+  }, 0);
 
   return (
     <div className={s.modal}>
       <div className={s.modalHead}>
-        <h2>Кошик: <span className={s.span}>{productsList.length} товари(-ів)</span></h2>
+        <h2>
+          Кошик:{" "}
+          <span className={s.span}>{productsList.length} товари(-ів)</span>
+        </h2>
         <img
           onClick={() => {
             setisOpen(false);
@@ -27,7 +28,7 @@ const ShoppingCart = ({ setProductsList, productsList, setisOpen }) => {
           alt="close"
         />
       </div>
-      <ul>
+      <ul className={s.purchaseList}>
         {productsList.map((product) => {
           const currentDish = dishes.find((dish) => {
             return product.id === dish.id;
@@ -39,7 +40,6 @@ const ShoppingCart = ({ setProductsList, productsList, setisOpen }) => {
               currentDish={currentDish}
               amount={product.amount}
               setProductsList={setProductsList}
-
             />
           );
         })}
@@ -51,7 +51,6 @@ const ShoppingCart = ({ setProductsList, productsList, setisOpen }) => {
         </div>
         <button className={s.button}>Зробити замовлення</button>
       </div>
-
     </div>
   );
 };
