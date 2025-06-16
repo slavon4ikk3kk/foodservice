07478c8bdfb0ac17f88226cdf36479e076fd90cb
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-06-16 22:11:22 +0300

    add logic for dinners

--- foodservice/src/pages/cabinet/Cabinet.jsx
+++ foodservice/src/pages/cabinet/Cabinet.jsx
@@ -5,15 +5,34 @@ import Dishes from "../../Data/dishes.jsx";
 
 const Cabinet = ({ setProductsList }) => {
   const [products, setProducts] = useState([]);
+  const [dinners, setDinners] = useState([]);
+  
   useEffect(() => {
     localStorage.setItem("sheetProducts", JSON.stringify(products));
   }, [products]);
   useEffect(() => {
-    fetch("")
+     localStorage.setItem("sheetDinners", JSON.stringify(dinners)); 
+  }, [dinners]);
+ 
+  useEffect(() => {
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxqoFtUA_dQltE6iAdqL5ytSTvsdk2MahA4pUdGK2zyM_pQb6iouhBJjbXTTyhqHrG_/exec"
+    )
       .then((res) => res.json())
       .then(setProducts)
-      .catch((err) => console.error("Помилка завантаження:", err));
+      .catch((err) => console.error("Помилка завантаження продуктів:", err));
   }, []);
+
+  
+  useEffect(() => {
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxpC6PKTu--agg0K8uIBwaXOMsTmPnhkNyCn_FdKh4QvBprXjrwOzKSmCbn5LJcgE-o/exec"
+    )
+      .then((res) => res.json())
+      .then(setDinners)
+      .catch((err) => console.error("Помилка завантаження обідів:", err));
+  }, []);
+
   return (
     <div style={{ padding: "90px 0" }} className="container">
       <ul className={s.list}>
