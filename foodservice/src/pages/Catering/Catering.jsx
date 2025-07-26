d8432db74c4c93d9758ce3c364a4579363935539
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-07-26 13:47:21 +0300

    add function

--- foodservice/src/pages/Catering/Catering.jsx
+++ foodservice/src/pages/Catering/Catering.jsx
@@ -10,7 +10,7 @@ const NAMES = {
   other: [],
 };
 
-const Catering = () => {
+const Catering = ({setProductsList}) => {
   const [catering, setCatering] = useState(() => {
     return [];
   });
@@ -37,11 +37,35 @@ const Catering = () => {
     setAmount(amount - 1);
   }
   function handleSubmit(e) {
-    e.preventDefault();
-    if (amount === 0) {
-      return;
+      e.preventDefault();
+      if (amount === 0) {
+        return;
+      }
+      setProductsList((prev) => {
+        const isInCart = prev.some((dishObj) => {
+          return dishObj.id === id;
+        });
+        if (isInCart) {
+          return prev.map((dishObj) => {
+            if (dishObj.id === id) {
+              return {
+                id,
+                amount: amount + dishObj.amount,
+                price: dishObj.price,
+                isDinner: "catering",
+                name: dishObj.name,
+              };
+            }
+            return dishObj;
+          });
+        }
+        return [...prev, { id, amount, price, isDinner: "catering", name }];
+      });
+      Notify.success("Товар доданий у кошик!", {
+        timeout: 1000,
+      });
+      setAmount(0);
     }
-  }
   useEffect(() => {
     fetch(
       "https://script.google.com/macros/s/AKfycby6Dw51AY84hUh2QHFN76XM3Jjj9jrCUe98dtzZI-lG_TWbBBOJpNbke_5zlwI37Ojx/exec"
@@ -91,59 +115,61 @@ const Catering = () => {
               {dishes.map((dish, index) => (
                 <li className={s.li} key={index}>
                   <span className={s.name}>{dish.name}</span>
-                  <div className={s.dishCart}>
-                    <div className={s.inputWrap}>
-                      <button
-                        onClick={handlePlus}
-                        className={s.plus}
-                        type="button"
-                      >
-                        <svg
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          width="24"
-                          height="24"
-                          class="sTccf4a"
+                  <form className={s.form} onSubmit={handleSubmit}>
+                    <div className={s.dishCart}>
+                      <div className={s.inputWrap}>
+                        <button
+                          onClick={handlePlus}
+                          className={s.plus}
+                          type="button"
                         >
-                          <path
-                            fill-rule="evenodd"
-                            d="M13,5 L13,12 L20,12 L20,13 L13,13 L13,20 L12,20 L11.999,13 L5,13 L5,12 L12,12 L12,5 L13,5 Z"
-                          ></path>
-                        </svg>
-                      </button>
-                      <button
-                        style={{ color: amount === 0 ? "gray" : "black" }}
-                        onClick={handleMinus}
-                        className={s.minus}
-                        type="button"
-                      >
-                        <svg
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          width="24"
-                          height="24"
-                          class="sTccf4a"
+                          <svg
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            width="24"
+                            height="24"
+                            class="sTccf4a"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M13,5 L13,12 L20,12 L20,13 L13,13 L13,20 L12,20 L11.999,13 L5,13 L5,12 L12,12 L12,5 L13,5 Z"
+                            ></path>
+                          </svg>
+                        </button>
+                        <button
+                          style={{ color: amount === 0 ? "gray" : "black" }}
+                          onClick={handleMinus}
+                          className={s.minus}
+                          type="button"
                         >
-                          <path
-                            fill-rule="evenodd"
-                            d="M20,12 L20,13 L5,13 L5,12 L20,12 Z"
-                          ></path>
-                        </svg>
-                      </button>
-                      <input
-                        type="number"
-                        step={0.5}
-                        onChange={HandleAmountChange}
-                        value={amount}
-                        min={0}
-                        className={s.input}
-                      ></input>
-                    </div>
+                          <svg
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            width="24"
+                            height="24"
+                            class="sTccf4a"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M20,12 L20,13 L5,13 L5,12 L20,12 Z"
+                            ></path>
+                          </svg>
+                        </button>
+                        <input
+                          type="number"
+                          step={0.5}
+                          onChange={HandleAmountChange}
+                          value={amount}
+                          min={0}
+                          className={s.input}
+                        ></input>
+                      </div>
 
-                    <button type="submit" className={s.purchaseButton}>
-                      додати до кошика
-                    </button>
-                  </div>
+                      <button type="submit" className={s.purchaseButton}>
+                        додати до кошика
+                      </button>
+                    </div>
+                  </form>
                   <div className={s.data}>
                     <p>
                       <strong>Вихід:</strong> {dish.weight}
