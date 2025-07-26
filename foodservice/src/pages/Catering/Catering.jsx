97b07d3c026b4d13012ce1b9ec1684e592f98cb7
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-07-26 12:18:32 +0300

    add button for catering

--- foodservice/src/pages/Catering/Catering.jsx
+++ foodservice/src/pages/Catering/Catering.jsx
@@ -22,7 +22,26 @@ const Catering = () => {
     fastFood: [],
     other: [],
   };
-
+   
+const [amount, setAmount] = useState(0);
+  function HandleAmountChange(e) {
+    setAmount(e.target.value);
+  }
+  function handlePlus() {
+    setAmount(amount + 1);
+  }
+  function handleMinus() {
+    if (amount === 0) {
+      return;
+    }
+    setAmount(amount - 1);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (amount === 0) {
+      return;
+    }
+}
   useEffect(() => {
     fetch(
       "https://script.google.com/macros/s/AKfycby6Dw51AY84hUh2QHFN76XM3Jjj9jrCUe98dtzZI-lG_TWbBBOJpNbke_5zlwI37Ojx/exec"
@@ -72,11 +91,61 @@ const Catering = () => {
           {dishes.map((dish, index) => (
             <li className={s.li} key={index}>
               <span className={s.name}>{dish.name}</span>
+              <div className={s.dishCart}>
+                        <div className={s.inputWrap}>
+                          <button onClick={handlePlus} className={s.plus} type="button">
+                            <svg
+                              viewBox="0 0 24 24"
+                              fill="currentColor"
+                              width="24"
+                              height="24"
+                              class="sTccf4a"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M13,5 L13,12 L20,12 L20,13 L13,13 L13,20 L12,20 L11.999,13 L5,13 L5,12 L12,12 L12,5 L13,5 Z"
+                              ></path>
+                            </svg>
+                          </button>
+                          <button
+                            style={{ color: amount === 0 ? "gray" : "black" }}
+                            onClick={handleMinus}
+                            className={s.minus}
+                            type="button"
+                          >
+                            <svg
+                              viewBox="0 0 24 24"
+                              fill="currentColor"
+                              width="24"
+                              height="24"
+                              class="sTccf4a"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M20,12 L20,13 L5,13 L5,12 L20,12 Z"
+                              ></path>
+                            </svg>
+                          </button>
+                          <input
+                            type="number"
+                            step={0.5}
+                            onChange={HandleAmountChange}
+                            value={amount}
+                            min={0}
+                            className={s.input}
+                          ></input>
+                        </div>
+              
+                        <button type="submit" className={s.purchaseButton}>
+                          додати до кошика
+                        </button>
+                      </div>
               <div className={s.data}>
                 <p><strong>Вихід:</strong> {dish.weight}</p>
                 <p><strong>Ціна:</strong> {dish.price}</p>
               </div>
             </li>
+
           ))}
         </ul>
       </React.Fragment>
